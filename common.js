0530eabae9896cdaeee1b496ae2b0d5b9b63f000
leticia kanno <106741944+Leticiakanno@users.noreply.github.com>
2024-11-26 13:44:20 -0300

    Delete common.js

--- common.js
+++
@@ -1,7 +0,0 @@
-// Função para buscar dados da API
-function fetchData(endpoint, callback) {
-    fetch(`http://localhost:3000/api/${endpoint}`)
-        .then(response => response.json())
-        .then(callback)
-        .catch(error => console.error('Erro ao buscar dados:', error));
-}
